c3e35220d867ce29bb769e8860fc56d8dffd1501
Tony Young <tony@MacBook.local>
2026-05-13 12:22:17 +1000

    Refactored use of Helmet and HelmetProvider in index.js, App.jsx, Routes.jsx, Article.jsx

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -13,6 +13,12 @@ export default function Root(){
 
     return (
         <>
+            <Helmet>
+                <title>Pixerious</title>
+                <meta name="description" content="Explore the captivating world of photography at Pixerious, your ultimate resource for everything from camera reviews and the latest in AI photography to historical insights and step-by-step editing tutorials. Whether you're a professional or a hobbyist, enrich your skills and passion for photography with our expert articles and guides" />
+                <meta name="keywords" content="photography news, latest cameras, AI photography techniques, photo editing tips, photography history, improve photography skills, camera reviews, technical guides for photographers, professional photography tips, digital photography, photography tutorials, portrait photography, landscape photography, sports photography, wedding photography, camera settings guide, photography lighting techniques, best photography practices, beginner photography, advanced photography techniques" />
+            </Helmet>
+            
             <Header />
             <Main />
             <Banner headings={news} id="news" />
